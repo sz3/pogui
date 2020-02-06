e49f247f501f9df09214e9c3c013ee86db2bc6a4
sz <sz@galacticicecube.com>
2020-02-05 23:25:03 -0600

    readme, moving __init__ to the righ place, basic setup.py

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-### Usage
+## Installation
+
+Use `pip`:
+```
+pip install pogui
+```
+
+or from source,
+```
+python setup.py build
+python setup.py install
+```
+
+## Usage
 
 python -m pogui.pogui
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pogui',
+    license='MIT',
+    url='https://github.com/sz3/pogui',
+    version='0.0.1',
+
+    entry_points={
+        'console_scripts': [
+            'pogui = pogui.pogui:main',
+        ],
+    },
+
+    packages=find_packages(exclude=('tests',)),
+    package_data={
+        'pogui': ['web/*.js', 'web/*.css', 'web/*.html', 'web/*/*.js', 'web/*/*.css'],
+    },
+
+    python_requires='>=3.6',
+    install_requires=[
+        'pogcli==0.1',
+        'PyQtWebEngine',
+        'PyQt5',
+        'pywebview',
+    ],
+
+    description='Pog User Interface',
+    long_description=(
+        'File encryption and backup UI.'
+    ),
+
+    author="Stephen Zimmerman",
+    author_email="sz@galacticicecube.com",
+
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
